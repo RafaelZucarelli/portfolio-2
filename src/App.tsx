f97e7e96a176d5a88ccf6aaaad5cb3afe5b20f02
RafaelZucarelli <zucarelli83@gmail.com>
2024-02-29 22:18:56 -0300

    addscrollToAboutSection

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faGithub, faGit, faJs, faReact,  faCss3 } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faGithub, faGit, faJs, faReact, faCss3 } from '@fortawesome/free-brands-svg-icons';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -27,6 +27,11 @@ const DigitarTexto = () => {
     return () => clearInterval(interval);
   }, [conteudo, velocidadeDigitacao]);
 
+  const scrollToAboutSection = () => {
+    const aboutSection = document.getElementById('sobre-mim');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
       <section id="servicos" className="caixa d-flex justify-content-center align-items-center vh-100">
@@ -42,37 +47,44 @@ const DigitarTexto = () => {
             <div className="col-md-4 text-light d-flex align-items-center">
               <div className="text-center">
                 <h2>{texto}</h2>
-                <a href="" className="btn btn-lg btn-outline-custom-purple">
+                <button onClick={scrollToAboutSection} className="btn btn-lg btn-outline-custom-purple">
                   Mais sobre mim
-                </a>
+                </button>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="d-flex justify-content-center align-items-center vh-100" style={{ backgroundColor: 'black' }}>
+      <section id="sobre-mim" className="d-flex justify-content-center align-items-center vh-100" style={{ backgroundColor: 'black' }}>
         <div className="container align-self-center">
           <div className="row">
             <div className="col-md-12 text-center text-light text-uppercase text=justify" style={{ lineHeight: '1.5' }}>
-              <h1 className='display-2'>SOBRE <span style={{ color: 'purple' }}>MIM</span></h1>
-              <p  className='mb-6'> 
+              <h1 className=''>SOBRE <span style={{ color: 'purple' }}>MIM</span></h1>
+              <p className='lead mb-6'> 
+                  <strong>
                     Desde muito jovem, fui atraído pela tecnologia, e foi por essa razão que escolhi ingressar na área de Sistemas de Informação. No entanto, desde o início, sempre ansiava por explorar mais profundamente o vasto e intrigante mundo da programação.      
                     Decidi transformar esse sonho em realidade, embarcando em uma jornada de
                     estudos dedicados ao Desenvolmento Web: <span>HTML, CSS, JavaScript e React</span>.
+                    </strong>
                 </p>
 
-                <p className='mb-6'>
+                <p className='lead mb-6'>
+                  
+                  <strong>
                     Com uma visão de longo prazo, no momento curso uma graduação em Sistemas de Informação e também,
                     estou imerso em cursos de Desenvolvimento Web para aprimorar ainda mais minhas
                     habilidades. Essa trajetória reflete meu comprometimento e determinação na transição de carreira
                     para a área de Desenvolvimento FrontEnd, contribuindo com inovação e criatividade para a vanguarda
                     da tecnologia.
+                    </strong>
                 </p >
 
-                <p className=''>
+                <p className='lead'>
+                  <strong>
                     Para além do código, e nos meus momentos de lazer, desfruto de
                     atividades como jogar video-game, mergulhar em boas leituras e apreciar filmes e séries.
+                  </strong>
                 </p>
                 <a href="" className="btn btn-lg btn-custom-purple">
                   BAIXAR CV
@@ -139,22 +151,18 @@ const DigitarTexto = () => {
         <h1 className='display-8 mb-4 py-4'>Educação</h1>
         </div>
             <ul className="education">
-                    <div className="year">
+                    <div className="year text-light">
                         <p><FontAwesomeIcon icon={faBriefcase} /> 2021 - Presente</p>
                     </div>
-                    <h5> Graduação - Sistemas de Informação <span>- Faculdade Instituto Superior do Litoral do Paraná </span></h5>
-                    <div className="year"> 
-                    <span><i className="fa-solid fa-briefcase"></i></span>
+                    <h5 className='text-light'> Graduação - Sistemas de Informação <span>- Faculdade Instituto Superior do Litoral do Paraná </span></h5>
+                    <div className="year text-light"> 
                         <p><FontAwesomeIcon icon={faBriefcase} /> 2024 - presente</p>
                     </div>
-                    <h5>Curso em Desenvolvimento Web <span>- Udemy</span></h5>
+                    <h5 className='text-light'>Curso em Desenvolvimento Web <span>- Udemy</span></h5>
             </ul>
             </div>
             </div>
-            
       </section>
-
-      {/* Adicione mais seções conforme necessário */}
     </>
   );
 }
