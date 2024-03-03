3ddedecf5debdeb15d068e08acb3cf1fbdc152c0
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-03 20:32:26 -0300

    addBottonMoreForMeComponent

---
+++ src/BottonMoreForMe.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BottonMoreForMe: React.FC = () => {
+  const scrollToAboutSection = () => {
+    const aboutSection = document.getElementById("sobre-mim");
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <button
+      onClick={scrollToAboutSection}
+      className="btn btn-lg btn-outline-custom-purple"
+    >
+      Mais sobre mim
+    </button>
+  );
+};
+
+export default BottonMoreForMe;
